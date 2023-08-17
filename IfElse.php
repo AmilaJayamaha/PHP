d2c79dee9081ba92ea3e015a3d8616fc0b6127f0
DESKTOP-P4FL1OC\Rasika <amilajayamaha96@gmail.com>
2023-08-17 10:11:55 +0530

    if else

---
+++ IfElse.php
@@ -0,0 +1,18 @@
+<!DOCTYpe html>
+<html lang="en">
+<head>
+   <meta charset ="UTF-8">
+   <title>If statements</tite>
+</head>
+<body>
+     <?php
+	  $avg_score = 50;
+	  if($avg_score>40){
+		  echo "You are qualified";
+	  }
+	  else{
+		  echo "Please try agian";
+	  }
+	  ?>
+</body>
+</html>
